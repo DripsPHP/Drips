7c69cfc84aa2917cff37ae9459eb34bf2a1d8556
Raffael Kessler <raffael.kessler@prowect.com>
2016-05-13 12:34:42 +0200

    Erste Schritte Möglichkeit (noch keine Routen)

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+use Drips\Routing\Router;
+use Drips\Routing\Error404Exception;
+
 define("INIT_FILE", __DIR__."/init.php");
 define("DRIPS_SRC", __DIR__."/src");
 define("ROUTES_FILE", DRIPS_SRC."/routes.php");
@@ -8,7 +11,15 @@ define("AUTOLOAD_FILE", DRIPS_SRC."/autoload.php");
 if(file_exists(INIT_FILE)){
     require_once INIT_FILE;
     if(file_exists(ROUTES_FILE)){
-        require_once ROUTES_FILE;
+        try {
+            require_once ROUTES_FILE;
+        } catch(Error404Exception $e) {
+            if(!Router::getInstance()->hasRoutes()){
+                die("Es wurden noch keine Routen eingetragen! Erste Schritte ...");
+            } else {
+                throw $e;
+            }
+        }
     }
     if(file_exists(AUTOLOAD_FILE)){
         require_once AUTOLOAD_FILE;
--- src/routes.php
+++ src/routes.php
@@ -8,7 +8,15 @@ $router = Router::getInstance();
 
 // --- Routen können hier angelegt werden ---
 
-
+/*
+$router->add("home", "/", function(){
+    $view = new Drips\MVC\View;
+    $view->assign("title", "Hello world!");
+    return $view->display("src/views/test.tpl");
+});
+*/
+//$router->add("less_compiler", "/less/{file}.css", Drips\LessCompiler\Controller::class);
+//$router->add("scss_compiler", "/scss/{file}.css", Drips\ScssCompiler\Controller::class);
 
 // ---
 
