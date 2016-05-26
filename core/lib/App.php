a8c4bf3a2217ba9078a307cef9b4e51dd48269ce
Raffael Kessler <raffael.kessler@prowect.com>
2016-05-26 22:52:03 +0200

    Routing ausgelagert

--- core/lib/App.php
+++ core/lib/App.php
@@ -12,29 +12,12 @@ class App extends Event
 
     public function __construct()
     {
-        $this->install();
         static::call("create", $this);
-        $this->router = Router::getInstance();
-    }
-
-    private function install()
-    {
-        // Apache?
-        if (PHP_SAPI != 'cli' && stripos($_SERVER['SERVER_SOFTWARE'], 'apache') !== false){
-            if (!in_array('mod_rewrite', apache_get_modules())) {
-                include(DRIPS_ERRORS."/mod_rewrite.phtml");
-            }
-        }
     }
 
     public function run()
     {
         static::call("startup", $this);
-        if($this->router->hasRoutes()){
-            $this->router->route();
-        } else {
-            include(DRIPS_ERRORS."/no_routes.phtml");
-        }
     }
 
     public function __destruct()
--- index.php
+++ index.php
@@ -1,14 +1,35 @@
 <?php
 
-// Drips
+// Drips-Konstanten ------------------------------------------------------------
 define('DRIPS_CORE', __DIR__.'/core');
 define('DRIPS_SRC', __DIR__.'/src');
 define('DRIPS_ERRORS', DRIPS_CORE.'/errors');
 define('DRIPS_PUBLIC', __DIR__.'/public');
+// -----------------------------------------------------------------------------
+
+
+// Benutzerdefinierte Fehlermeldungen für Exceptions ---------------------------
 
 // Wurde bereits `composer update` durchgeführt?
 if(!@include('vendor/autoload.php')){
     include(DRIPS_ERRORS.'/install_composer.phtml');
 }
+
+use Drips\Debugger\Handler;
+
+// mod_rewrite aktiviert?
+Handler::on('Drips\Routing\ModRewriteNotEnabledException', function(){
+    include DRIPS_ERRORS.'/mod_rewrite.phtml';
+    return true;
+});
+
+// sind bereits Routen registriert?
+Handler::on('Drips\Routing\NoRoutesException', function(){
+    include DRIPS_ERRORS.'/no_routes.phtml';
+    return true;
+});
+
+// -----------------------------------------------------------------------------
+
 
 include(DRIPS_SRC.'/bootstrap.php');
