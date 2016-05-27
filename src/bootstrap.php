2f3402889eb37ba2d868cdc408e088748a39de2a
Raffael Kessler <raffael.kessler@prowect.com>
2016-05-27 22:45:03 +0200

    Logger hinzugefügt (zum Testen)

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,10 +2,22 @@
 
 $app = new Drips\App;
 
+// !!! ONLY FOR TESTING
+$app->logger->addDebug('Nur zum Debuggen');
+$app->logger->addInfo('Das ist eine Info');
+$app->logger->addNotice('Das ist ein Notice');
+$app->logger->addError('Das ist ein Error');
+$app->logger->addWarning('Das ist ein Warning');
+$app->logger->addCritical('Das ist ein Critical');
+$app->logger->addAlert('Das ist ein Alert');
+$app->logger->addEmergency('Das ist ein Emergency');
+// ----------------------------------------
+
 $app->router->add("home", "/", function(){
     echo "It Works!";
     echo "<a href='".routeLink("test")."'>Test</a>";
 });
+
 
 $app->router->add("test", "/test", function(){
     echo "<a href='".routeLink("home")."'>Zur&uuml;ck</a>";
