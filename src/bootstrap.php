ee844e44e7cc57198cd07b5f4a4455de5520d637
Raffael Kessler <raffael.kessler@prowect.com>
2016-05-28 22:55:25 +0200

    RESET

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,25 +2,13 @@
 
 $app = Drips\App::getInstance();
 
-// !!! ONLY FOR TESTING
-$app->logger->addDebug('Nur zum Debuggen');
-$app->logger->addInfo('Das ist eine Info');
-$app->logger->addNotice('Das ist ein Notice');
-$app->logger->addError('Das ist ein Error');
-$app->logger->addWarning('Das ist ein Warning');
-$app->logger->addCritical('Das ist ein Critical');
-$app->logger->addAlert('Das ist ein Alert');
-$app->logger->addEmergency('Das ist ein Emergency');
-// ----------------------------------------
-
-$app->router->add("home", "/", function(){
-    echo "It Works!";
-    echo "<a href='".route("test")."'>Test</a>";
-});
+// =============================================================================
+// ==                Hier kannst du deine Routen registrieren                 ==
+// =============================================================================
 
 
-$app->router->add("test", "/test", function(){
-    echo "<a href='".route("home")."'>Zur&uuml;ck</a>";
-});
+
+
+// =============================================================================
 
 $app->run();
