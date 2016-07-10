892402d0f38ac6a7914f664fd9b21c3d721e6b2a
Raffael Kessler <raffael.kessler@prowect.com>
2016-07-10 14:42:56 +0200

    Mail-Config hinzugefügt

--- config/dev.config.php
+++ config/dev.config.php
@@ -1,20 +1,29 @@
 <?php
 
 return array(
-
+    // Database
     'database_type' => 'mysql',
-
     'database_host' => 'localhost',
-
     'database_name' => 'drips',
-
     'database_username' => 'root',
-
     'database_password' => 'root',
-
     'database_port' => 3306,
-
     'database_charset' => 'utf8',
+    'database_logging' => true,
 
-    'timezone' => 'Europe/Vienna'
+    // Localization
+    'timezone' => 'Europe/Vienna',
+
+    // Mail SMTP
+    'mail_smtp' => false,
+    'mail_smtp_host' => '',
+    'mail_smtp_username' => '',
+    'mail_smtp_password' => '',
+    'mail_smtp_secure' => '',
+    'mail_smtp_port' => '',
+    // Mail 
+    'mail_from_email' => '',
+    'mail_from_name' => '',
+    'mail_cc' => [],
+    'mail_bcc' => []
 );
