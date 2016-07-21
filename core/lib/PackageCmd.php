b8b4fea6b63b919cf45fac733f9729ce36131675
Raffael Kessler <raffael.kessler@prowect.com>
2016-07-21 09:15:54 +0200

    Bugfixes bzgl. Composer

--- core/lib/PackageCmd.php
+++ core/lib/PackageCmd.php
@@ -8,36 +8,47 @@ use Drips\CLI\Console;
 abstract class PackageCmd implements ICommand
 {
 
+    protected static function composer()
+    {
+        $composer_path = 'composer.phar';
+        if (file_exists($composer_path)) {
+            return 'php ' . $composer_path;
+        }
+        return 'composer';
+    }
+
     public static function install($package = null, $version = '')
     {
-        if($package == null){
+        if ($package == null) {
             Console::error('Du musst ein Package angeben');
-        }else{
-            if(strpos($package, '/') === false){
-                $package = 'drips/'.$package;
+        } else {
+            if (strpos($package, '/') === false) {
+                $package = 'drips/' . $package;
             }
-            shell_exec('composer require '.$package.' '.$version);
+            shell_exec(static::composer() . ' require ' . $package . ' ' . $version);
         }
     }
 
     public static function uninstall($package = null)
     {
-        if($package == null){
+        if ($package == null) {
             Console::error('Du musst ein Package angeben');
-        }else{
-            if(strpos($package, '/') === false){
-                $package = 'drips/'.$package;
+        } else {
+            if (strpos($package, '/') === false) {
+                $package = 'drips/' . $package;
             }
-            shell_exec('composer remove '.$package);
+            shell_exec(static::composer() . ' remove ' . $package);
         }
     }
 
-    public static function installed(){
-        echo shell_exec('composer show -i 2>&1');
+    public static function installed()
+    {
+        echo shell_exec(static::composer() . ' show 2>&1');
     }
 
-    public static function available(){
-        echo shell_exec('composer search drips 2>&1');
+    public static function available()
+    {
+        echo shell_exec(static::composer() . ' search drips 2>&1');
     }
 
 
